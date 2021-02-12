9f6bf3eb38f575966e1e015c7a25242192c3868c
Akhilesh Shastri <as14g18@soton.ac.uk>
2021-02-12 23:04:40 +0000

    add donate and donations endpoints

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import requests
 import json
 import urllib.request
 
@@ -13,6 +12,36 @@ def find_food_banks():
     with urllib.request.urlopen(f"https://www.givefood.org.uk/api/2/foodbanks/search/?address={postcode}") as url:
         data = json.loads(url.read().decode())
     return jsonify(data)
+
+
+@app.route("/donate", methods=["POST"])
+def donate():
+    data = request.json
+    print(data)
+
+    with open("donations.json", "r") as f:
+        donations = json.load(f)
+
+    username = data["username"]
+    if username not in donations:
+        donations[username] = []
+    
+    donations[username].append(data)
+
+    with open("donations.json", "w") as f:
+        json.dump(donations, f)
+
+    return jsonify({200: "success"})
+
+
+@app.route("/donations", methods=["GET"])
+def donations():
+    username = request.args.get("username")
+
+    with open("donations.json", "r") as f:
+        donations = json.load(f)
+
+    return jsonify({"data": donations[username]})
 
 
 def main():
